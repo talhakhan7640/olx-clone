1c824eadb6d19673773b778126a048e59ee2af87
talhakhan7640 <talhakhan7640@gmail.com>
2023-09-09 21:20:02 +0530

    Fixed code for warnings like user or email already exist

--- controller/authentication.js
+++ controller/authentication.js
@@ -1,68 +1,64 @@
 import userModel from "../models/authentication.js";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const userRegistrationController = (request, response) => {
+export const userRegistrationController = async (request, response) => {
   const saltRounds = 10;
   const username = request.body.username;
   const userEmail = request.body.email;
   const userPassword = request.body.password;
 
-userModel.findOne({ username: username })
-.then( (err, document ) => {
-    if (document) {
-      if (userEmail == document.email) {
-        response.status(409).json({ message: 'Username and Email already exist!' })
-      } else {
-        return response.send({
-          message: "Username already exist",
-        });
-      }
+  const document = await userModel.findOne({ username: username });
+  if (document) {
+    if (userEmail == document.email) {
+      response
+        .status(409)
+        .send({ message: "Username and Email already exist!" });
     } else {
-      userModel.findOne({ email: userEmail })
-      .then(( err, document ) => {
-
-        if (document) {
-          return response.send({
-            message: "Email already exist",
+      return response.status(409).send({
+        message: "Username already exist",
+      });
+    }
+  } else {
+    const docEmail = await userModel.findOne({ email: userEmail });
+    if (docEmail) {
+      return response.send({
+        message: "Email already exist",
+      });
+    } else {
+      bcrypt
+        .hash(userPassword, saltRounds)
+        .then((hashedPassword) => {
+          var user = new userModel({
+            username: username,
+            email: userEmail,
+            password: hashedPassword,
           });
-        } else {
-          bcrypt
-            .hash(userPassword, saltRounds)
-            .then((hashedPassword) => {
-              const user = new userModel({
-                username: username,
-                email: userEmail,
-                password: hashedPassword,
+          user
+            .save()
+            .then((result) => {
+              response.status(201).send({
+                message: "User has been created successfully!",
+                result,
               });
-
-              user
-                .save()
-                .then((result) => {
-                  response.status(201).send({
-                    message: "User has been created successfully!",
-                    result,
-                  });
-                })
-                .catch((error) => {
-                  response.status(500).send({
-                    message: "Something went wrong! User could not be created!",
-                    error,
-                  });
-                });
             })
-            .catch((e) => {
+            .catch((error) => {
               response.status(500).send({
-                message:
-                "Something went wrong! Password could not be hashed successfully!",
-                e,
+                message: "Something went wrong! User could not be created!",
+                error,
               });
             });
-        }
-      } )
-} })
-}
-
+        })
+        .catch((e) => {
+          response.status(500).send({
+            message:
+              "Something went wrong! Password could not be hashed successfully!",
+            e,
+          });
+        });
+    }
+  }
+};
 
 export const userLoginController = (request, response) => {
   const username = request.body.username;
